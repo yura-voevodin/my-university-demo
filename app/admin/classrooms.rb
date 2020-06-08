368eb224108bbe6b61468d99814a0a61fe775c7a
Yura Voevodin <voevodin.yura@icloud.com>
2020-06-08 10:52:08 +0300

    "ActiveAdmin" resources for Classroom, Group, Teacher

---
+++ app/admin/classrooms.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register Classroom do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  
+  permit_params :name, :lowercase_name
+
+  form do |f|
+    f.inputs do
+      f.input :name
+    end
+    f.actions
+  end
+  
+end
