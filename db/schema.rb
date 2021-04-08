f12453426c1217017ce15f64c27a37ab1cba4b11
Yura Voevodin <voevodin.yura@icloud.com>
2021-04-08 21:46:40 +0300

    Add migrations for ActiveStorage (after command `rails app:update`)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_183755) do
+ActiveRecord::Schema.define(version: 2021_04_08_184219) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
